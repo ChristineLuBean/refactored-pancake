c1d4922e8323b8aac541638d143e7d97bb9c09e4
Christine LuBean <christinemlubean@gmail.com>
2022-04-13 12:30:33 -0700

    stockTicker.js updated to add a object property into the DOM

--- assets/js/stockTicker.js
+++ assets/js/stockTicker.js
@@ -1,29 +1,25 @@
-let getStock = function(){
-  fetch('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a90901c0235bacb77346225fa7314ebc')
-  .then(res => res.json())
-  .then(data => {
-      console.log(data)
-
-  })
-
-  data.forEach(stockItem => {
-    let stockList = document.querySelector('.#ticker01')
-
-    let stock = document.createElement('li');
-    stock.innerText = data.symbol;
-
-    stockList.appendChild(stock);
-
-})
-
-  .catch(err =>
-      console.log(`error ${err}`)
+let getStock = function () {
+  fetch(
+    "https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a90901c0235bacb77346225fa7314ebc"
   )
-}
+    .then((res) => res.json())
+    .then((data) => {
+      let stockList = document.querySelector("#ticker01");
+      let i = 0;
 
-getStock()
+      data
+        .forEach((stockItem) => {
+          i++;
+          //
 
+          let stock = document.createElement("li");
+          stock.setAttribute("id", `stock${i}`);
+          stock.innerText = data[i].symbol;
 
+          stockList.appendChild(stock);
+        })
 
-
-
+        .catch((err) => console.log(`error ${err}`));
+    });
+};
+getStock();
