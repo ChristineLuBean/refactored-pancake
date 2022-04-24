2872d1821e006ad56d5ab6371c4f50470ce8ff2f
Christine LuBean <christinemlubean@gmail.com>
2022-04-23 22:28:21 -0700

    Got ticker to display all stock info and to scroll.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,23 +17,23 @@ jQuery(document).ready(function() {
         cssEase: 'linear'
     });
     // latest post slider call 
-    $('.latest_postnav').newsTicker({
-        row_height: 64,
-        speed: 800,
-        prevButton: $('#prev-button'),
-        nextButton: $('#next-button')
-    });
-    jQuery(".fancybox-buttons").fancybox({
-        prevEffect: 'none',
-        nextEffect: 'none',
-        closeBtn: true,
-        helpers: {
-            title: {
-                type: 'inside'
-            },
-            buttons: {}
-        }
-    });
+    // $('.latest_postnav').newsTicker({
+    //     row_height: 64,
+    //     speed: 800,
+    //     prevButton: $('#prev-button'),
+    //     nextButton: $('#next-button')
+    // });
+    // jQuery(".fancybox-buttons").fancybox({
+    //     prevEffect: 'none',
+    //     nextEffect: 'none',
+    //     closeBtn: true,
+    //     helpers: {
+    //         title: {
+    //             type: 'inside'
+    //         },
+    //         buttons: {}
+    //     }
+    // });
     // jQuery('a.gallery').colorbox();
     //Check to see if the window is top if not then display button
     $(window).scroll(function() {
--- assets/js/stockTicker.js
+++ assets/js/stockTicker.js
@@ -1,24 +1,23 @@
-let getStock = function () {
+// const getStock = function () {
   fetch(
-    "https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a90901c0235bacb77346225fa7314ebc"
-  )
+    "https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=a90901c0235bacb77346225fa7314ebc")
     .then((res) => res.json())
     .then((data) => {
-      let stockList = document.querySelector("#ticker01");
-      let i = 0;
+ 
+      //array of stocks
+      const stocks = [];
+      let tickerText = '';
 
-      data
-        .forEach((stockItem) => {
-          i++;
+    for(let i = 0; i < data.length; i++){
+      tickerText += '<strong>' + data[i].symbol + '</strong>' + ' ' + data[i].price + ' ' + data[i].change + ' ' + data[i].changesPercentage + ' '
+    }
 
-          let stock = document.createElement("li");
-          stock.setAttribute("id", `stock${i}`);
-          stock.innerText = data[i].symbol;
+    document.querySelector("#scroll").innerHTML = tickerText;
 
-          stockList.appendChild(stock);
-        })
+    
+  });
 
-        .catch((err) => console.log(`error ${err}`));
-    });
-};
-getStock();
+// }  
+// getStock();
+
+
